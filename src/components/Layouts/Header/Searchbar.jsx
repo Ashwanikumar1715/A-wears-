dfaad07a57e4421cb3a02faec14485f2e36e4199
ashwani kumar <ashwanikumar01011997123@gmail.com>
2024-03-30 12:50:55 +0530

    new commit

--- src/components/Layouts/Header/Searchbar.jsx
+++ src/components/Layouts/Header/Searchbar.jsx
@@ -18,7 +18,7 @@ const Searchbar = () => {
 
     return (
         <form onSubmit={handleSubmit} className="w-3/3 sm:w-8/12 px-2.5 sm:px-10 py-2.5 flex  items-center shadow-md  rounded-lg">
-        <input value={keyword} onChange={(e) => setKeyword(e.target.value)} className="text-sm w-full flex-1 outline-none border-none placeholder-gray-500" type="text" placeholder="Search farm byproducts" />
+        <input value={keyword} onChange={(e) => setKeyword(e.target.value)} className="text-sm w-full flex-1 outline-none border-none placeholder-gray-500" type="text" placeholder="Search products" />
         <button type="submit" className="text-[#6a09e8] relative " aria-label="Click me to submit the form"><SearchIcon /></button>
     </form>
     );
